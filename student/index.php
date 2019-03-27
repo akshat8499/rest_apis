18b26c60da0e78dfd9ffe26a7a705478830c8278
akshat8499 <47627883+akshat8499@users.noreply.github.com>
2019-03-27 17:23:27 +0530

    Delete index.php

--- student/index.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-    <li><a href="read.php"><strong>Read</strong></a> - find a user</li>
-    <li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
-    <li><a href="delete.php"><strong>Delete</strong></a> - remove a user</li>
-</ul>
