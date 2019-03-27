a3453163b2eb55a91a7343cf61cc95c302a9afe9
akshat8499 <47627883+akshat8499@users.noreply.github.com>
2019-03-27 17:21:33 +0530

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><a href="student/index.php"><strong>Student</strong></a> - CRUD on Student table</li>
-    <li><a href="user/index.php"><strong>User</strong></a> - CRUD on User table</li>
-    <li><a href="employee/index.php"><strong>Employee</strong></a> - CRUD on Employee table</li>
-</ul>
